5cc6d0e982012d569f6fcf8d87e9bd969f87360c
JCriz01 <skydoesgta802@gmail.com>
2023-10-06 18:40:45 -0400

    creating base branch

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,12 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			textShadow: {
-				DEFAULT: '3px 4px 0px rgba(0, 0, 0, 0.25);',
+			fontFamily: {
+				gilroylight: ['Gilroy-Light', 'sans-serif'],
+				gilroyextrabold: ['Gilroy-ExtraBold', 'sans-serif'],
+			},
+			backgroundColor: {
+				'darkmode': '#222A36'
 			},
 		},
 	},
