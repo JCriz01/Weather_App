390904c182fd697be4dbbbc314a2cc410a7be108
JCriz01 <skydoesgta802@gmail.com>
2023-10-13 20:31:41 -0400

    Adding DOM integration for weather API info

--- src/scripts/dom.ts
+++ src/scripts/dom.ts
@@ -1,22 +1,127 @@
-function createDisplayer(): void {
-	const container = document.querySelector('body');
+export function createFirstTimePopUp() {
+	const bodyElem = document.querySelector('body');
+	const popUpElem = document.createElement('div');
+	popUpElem.classList.add(
+		'relative',
+		'h-4/5',
+		'w-2/3',
+		'self-center',
+		'flex',
+		'flex-col',
+		'items-center',
+		'rounded-lg',
+		'bg-zinc-900',
+		'p-5'
+	);
+	popUpElem.setAttribute('id', 'first-time-displayer');
 
-	const elementBody = document.createElement('div');
-	elementBody.classList.add('relative', 'w-2/3');
+	const headerElem = document.createElement('h1');
+	headerElem.classList.add('pb-5', 'text-xl');
+	headerElem.textContent = 'Welcome to SkySavvy Weather App';
+
+	const pInfoElem = document.createElement('p');
+	pInfoElem.classList.add('pb-3', 'text-center');
+
+	const pInfo2Elem = document.createElement('p');
+	pInfo2Elem.classList.add('text-center');
+	pInfo2Elem.textContent =
+		'Weather display is currently empty, to search for an area with accurate date information press the Search button';
+	const popUpBtn = document.createElement('button');
+	popUpBtn.classList.add(
+		'rounded-md',
+		'px-3',
+		'py-1',
+		'text-base',
+		'hover:scale-105',
+		'active:translate-y-1'
+	);
+	popUpBtn.setAttribute(
+		'style',
+		'background: rgb(228,94,94); background: linear-gradient(198deg, rgba(228,94,94,1) 14%, rgba(51,136,138,1) 100%);'
+	);
+
+	popUpBtn.setAttribute('id', 'first-time-view-btn');
+
+	popUpBtn.textContent = 'Sounds Good!';
+
+	popUpElem.appendChild(headerElem);
+	popUpElem.appendChild(pInfoElem);
+	popUpElem.appendChild(pInfo2Elem);
+	popUpElem.appendChild(popUpBtn);
+
+	bodyElem?.appendChild(popUpElem);
+	bodyElem?.insertBefore(popUpElem, bodyElem.lastElementChild);
 }
 
-export function firstTimeDisplayer(): void {
+export function deleteFirstTimePopUp() {
+	const targetElem = document.querySelector('#first-time-displayer');
+	console.log('removing', targetElem);
+	targetElem?.remove();
+}
+
+export async function createLoadingIndicator() {
+	const bodyElem = document.querySelector('body');
+	const container = document.createElement('div');
+
+	const imgElem = document.createElement('img');
+
+	const textElem = document.createElement('p');
+	textElem.classList.add('text-lg');
+
+	container.classList.add(
+		'flex',
+		'w-1/2',
+		'justify-evenly',
+		'self-center',
+		'absolute',
+		'bottom-1/2',
+		'bg-emerald-200',
+		'text-black',
+		'rounded-lg',
+		'p-4'
+	);
+	imgElem.classList.add('animate-spin', 'w-7');
+	imgElem.src = '/Weather_App/spinningArrows.svg';
+
+	textElem.textContent = 'Loading...';
+
+	container.appendChild(imgElem);
+	container.appendChild(textElem);
+
+	bodyElem?.insertBefore(container, bodyElem.lastElementChild);
+}
+
+export function deleteLoadingIndicator() {
+	const bodyElem = document.querySelector('body');
+
+	const targetElem = bodyElem?.children[1];
+
+	bodyElem?.removeChild(bodyElem?.children[1]!);
+	console.log(bodyElem?.children);
+
+	console.log('removing', targetElem);
+}
+
+export function DisplayFirstTimePopUp(): void {
 	const displayer = document.querySelector('#first-time-displayer');
-	const firstTimeBtn = document.querySelector('#first-time-btn');
+	const closeHeaderBtn = document.querySelector('#close-header-btn');
+	const searchBtn = document.querySelector('#weather-search');
 
-	if (FirstVisit()) {
-		displayer?.classList.remove('hidden');
-	} else {
-		displayer?.classList.add('hidden');
-	}
+	createFirstTimePopUp();
+	const firstTimeBtn = document.querySelector('#first-time-view-btn');
+
+	closeHeaderBtn?.classList.remove('animate-pulse', 'active:translate-y-1');
+	searchBtn?.classList.remove('active:translate-y-1');
+
+	firstTimeBtn?.addEventListener('click', () => {
+		closeHeaderBtn?.classList.add('animate-pulse', 'active:translate-y-1');
+		searchBtn?.classList.add('active:translate-y-1');
+
+		deleteFirstTimePopUp();
+	});
 }
 
-function FirstVisit(): boolean {
+export function isFirstVisit(): boolean {
 	if (!localStorage.getItem('firstVisit')) {
 		localStorage.setItem('firstVisit', 'true');
 		return true;
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,26 +1,29 @@
 import { weatherObj, weather } from './weather';
-import { firstTimeDisplayer } from './dom';
+import {
+	isFirstVisit,
+	createLoadingIndicator,
+	deleteLoadingIndicator,
+	createFirstTimePopUp,
+	deleteFirstTimePopUp,
+	DisplayFirstTimePopUp
+} from './dom';
 
 const openBtn = document.getElementById('open-header-btn');
 
 const container = document.getElementById('header-container');
-
+const closeHeaderBtn = document.getElementById('close-header-btn');
 const weatherBtn = document.getElementById('weather-search');
+const weatherDataElem = document.getElementById('weather-data');
+const currWeather = document.querySelector('#current-weather-wrapper');
+const forecastElem = document.querySelector('#forecast-wrapper');
 
 //get only input in html
 const weatherInput = document.querySelector('input');
-
-openBtn?.addEventListener('click', (Event) => {
-	openBtn?.classList.toggle('hidden');
-
-	container?.classList.toggle('hidden');
-});
 
 async function getWeatherAPI(locationString: string | undefined) {
 	const weatherJson = await weather(locationString);
 
 	//replacing this with the function that will update the weather object
-
 	weatherObj.imageURL = weatherJson.current.condition.icon;
 	weatherObj.location.country = weatherJson.location.country;
 	weatherObj.location.name = weatherJson.location.name;
@@ -32,11 +35,61 @@ async function getWeatherAPI(locationString: string | undefined) {
 	console.log('This is the object from weatherJson', weatherJson);
 	console.log('this is the object that we are using ', weatherObj);
 }
-
-weatherBtn?.addEventListener('click', (Event) => {
-	const weatherLocation = weatherInput?.value;
-	getWeatherAPI(weatherLocation);
-});
-
+//---------------------------------------------------------
 //refactoring everything above this line
-firstTimeDisplayer();
+
+function isInputValid(input: HTMLInputElement | null): boolean {
+	if (input?.validity.valueMissing) {
+		input.setCustomValidity('Please enter a location');
+	} else if (input?.validity.patternMismatch) {
+		input.setCustomValidity('Please enter a valid location');
+	} else {
+		input?.setCustomValidity('');
+	}
+
+	return input?.checkValidity() ?? false;
+}
+
+
+function main() {
+	if (isFirstVisit()) {
+		DisplayFirstTimePopUp();
+	}
+
+	weatherBtn?.addEventListener('click', (Event) => {
+		console.log('clicked');
+
+		if (isInputValid(weatherInput)) {
+			Event.preventDefault();
+
+			const loadingIndicatiorPromise = createLoadingIndicator();
+			const weatherPromise = getWeatherAPI(weatherInput?.value);
+
+			Promise.all([loadingIndicatiorPromise, weatherPromise])
+				.then(() => {
+					deleteLoadingIndicator();
+					weatherDataElem?.classList.remove('hidden');
+					currWeather?.classList.remove('hidden');
+					forecastElem?.classList.remove('hidden');
+				})
+				.catch(() => {
+					//TODO: add error handling
+					console.log('failed');
+				});
+		}
+	});
+
+	//TODO: add logic to disable hiding the header BEFORE the weather data is loaded from the API
+	openBtn?.addEventListener('click', (Event) => {
+		openBtn?.classList.toggle('hidden');
+
+		container?.classList.toggle('hidden');
+	});
+
+	closeHeaderBtn?.addEventListener('click', () => {
+		container?.classList.toggle('hidden');
+		openBtn?.classList.toggle('hidden');
+	});
+}
+
+main();
